133724a2ee9a6d17030906b43973ebed354066a5
knguyennt <khoanguyennt2010@gmail.com>
2025-05-23 23:59:32 +0700

    feat: add lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+    end
+
+  }
+}
